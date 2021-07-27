0e481eb04512b8b5ef97c40d4d1a0336a5bac0a2
Ella Choy <egal21de@gmail.com>
2021-07-28 01:52:33 +0200

    proj

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+console.log("hallo neues projekt!");
+
+function userInputed(){
+    console.log(document.getElementById("Name").value);
+    console.log(document.getElementById("Email").value);
+    console.log(document.getElementById("Message").value);
+
+}
